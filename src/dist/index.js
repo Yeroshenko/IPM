47e8e38c9c73f890a49c5dfda1af286c384a1f9c
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-08-22 17:13:48 +0300

    feat: add storybook

--- src/dist/index.js
+++ src/dist/index.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 var react_1 = require("react");
 var react_dom_1 = require("react-dom");
-require("./index.sass");
+require("styles/index.sass");
 var App_1 = require("./App");
 var serviceWorker = require("./serviceWorker");
 react_dom_1["default"].render(react_1["default"].createElement(react_1["default"].StrictMode, null,
